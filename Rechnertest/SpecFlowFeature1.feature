b12cff1e898e973e90cfee71f37b41e6e32c7206
wi19b027 <wi19b027@technikum-wien.at>
2020-12-03 16:31:07 +0100

    1

---
+++ Rechnertest/SpecFlowFeature1.feature
@@ -0,0 +1,21 @@
+﻿Feature: SpecFlowFeature1
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be have a Taschenrechner
+
+@mytag
+
+Scenario: sinus vom Zahl
+	Given the number is Pi
+	When the sinus of number
+	Then the result should be 0
+
+Scenario: cosinus vom Zahl
+	Given the number is Pi
+	When the cosinus of number
+	Then the result should be -1
+
+Scenario: tangens vom Zahl
+	Given the number is Pi
+	When the tangens of number
+	Then the result should be 0
---
+++ Rechnertest/SpecFlowFeature1Steps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Rechnertest
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private double x, y;
+        [Given(@"the number is Pi")]
+        public void GivenTheNumberIsPi()
+        {
+            x = Math.PI;
+        }
+        
+        [When(@"the sinus of number")]
+        public void WhenTheSinusOfNumber()
+        {
+            y = Math.Sin(x);
+        }
+        
+        [When(@"the cosinus of number")]
+        public void WhenTheCosinusOfNumber()
+        {
+            y = Math.Cos(x);
+        }
+        
+        [When(@"the tangens of number")]
+        public void WhenTheTangensOfNumber()
+        {
+            y = Math.Tan(x);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            var expected = p0;
+            var actual = y;
+            Assert.Equal(expected, actual, 5);
+        }
+    }
+}
